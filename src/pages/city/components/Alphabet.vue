1671201a27fc2279cbf97f0348ce0eeece887504
zhanghuajin12345 <13521566900@163.com>
2018-08-15 19:52:09 +0800

    性能优化，添加函数节流方法

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -20,8 +20,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   mounted () {
     // console.log(this.cities)
@@ -43,16 +48,20 @@ export default {
       this.touchStatus = true
     },
     handleLetterMove (e) {
-      console.log(e.touches[0])
+      // console.log(e.touches[0].target.innerText)
       // this.$emit('change', e.target.innerText)
-      if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
-        }
+      if (this.timer) {
+        clearTimeout(this.timer)
       }
+      this.timer = setTimeout(() => {
+        if (this.touchStatus) {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }
+      }, 16)
     },
     handleLetterEnd (e) {
       this.touchStatus = false

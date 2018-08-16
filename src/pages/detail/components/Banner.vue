a92e356da394ad2e9303ce322acbf233f327233d
zhanghuajin12345 <13521566900@163.com>
2018-08-16 17:50:28 +0800

    点击图片放大

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="banner">
-    <div class="bannerImg-wrapper">
+  <div class="banner" >
+    <div class="bannerImg-wrapper" @click="handleBannerClick">
       <swiper :options="swiperOption">
         <swiper-slide v-for="(item,index) of list" :key="index">
           <img :src="item" alt="">
@@ -12,11 +12,16 @@
       <li>产品编号</li>
       <li><span class="iconfont banner-icon">&#xe692;</span>{{listNum}}</li>
     </ul>
+    <common-gallary v-show="isShowGallary" :list='this.list' @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 <script>
+import CommonGallary from '@common/gallary/Gallary'
 export default {
   name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
   data () {
     return {
       list: ['https://img1.qunarzz.com/p/tts9/1708/23/8e372e48c656b102.jpg_r_640x420x90_9b14366e.jpg', 'https://img1.qunarzz.com/p/tts4/1801/bd/b1d05fc1258e8802.jpg_r_640x420x90_d391b0f3.jpg'],
@@ -24,12 +29,21 @@ export default {
         loop: true,
         pagination: '.swiper-pagination',
         paginationType: 'fraction'
-      }
+      },
+      isShowGallary: false
     }
   },
   computed: {
     listNum () {
       return this.list.length
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.isShowGallary = true
+    },
+    handleGallaryClose () {
+      this.isShowGallary = false
     }
   }
 }
